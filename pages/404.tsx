e6312f8d88d76f8bf5e46448ece29fc4e448523f
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-19 18:28:31 +0900

    404 redirectioin

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,12 @@
-import { useRouter } from "next/router";
-
 const Custom404 = () => {
   if (typeof window === "undefined") return null;
-  const history = useRouter();
-  const path = history.asPath;
+  const path = window.location.pathname;
   const splitPath = path.split("/");
 
-  if (splitPath[1] === "userInfo") return history.replace(path);
-  history.replace("/");
+  if (splitPath[1] === "userInfo" && splitPath.length === 3)
+    return window.location.replace(path);
+
+  window.location.replace("/");
 
   return <div>404입니다</div>;
 };
