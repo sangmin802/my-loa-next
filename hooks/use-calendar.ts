c79cbfec0e69945456c21937857ad67c63e047d5
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-19 17:32:25 +0900

    useCalendar initialData에 따른 옵션

--- hooks/use-calendar.ts
+++ hooks/use-calendar.ts
@@ -1,14 +1,19 @@
 import { getCalendarData } from "api/api";
+import { useMemo } from "react";
 import { useQuery } from "react-query";
 
 export function useCalendar(rerenderKey, initialData) {
+  const option = useMemo(() => {
+    const root = { refetchOnWindowFocus: false };
+
+    if (initialData) return { ...root, initialData: JSON.parse(initialData) };
+    return root;
+  }, [initialData]);
+
   const { data: calendarData } = useQuery(
     ["fetchCalendarData", rerenderKey],
     () => getCalendarData(),
-    {
-      initialData: JSON.parse(initialData),
-      refetchOnWindowFocus: false,
-    }
+    option
   );
 
   return calendarData;
