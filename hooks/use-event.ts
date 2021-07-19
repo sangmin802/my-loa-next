f707a2f30a4b2d52e6ad9d9d9d5d9518f79ba8df
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-19 13:49:31 +0900

    useEvent initialData에 따른 조건부 옵션

--- hooks/use-event.ts
+++ hooks/use-event.ts
@@ -1,11 +1,20 @@
 import { getEventData } from "api/api";
+import { useMemo } from "react";
 import { useQuery } from "react-query";
 
 export function useEvent(initialData) {
-  const { data: eventData } = useQuery("fetchEventData", () => getEventData(), {
-    initialData: JSON.parse(initialData),
-    refetchOnWindowFocus: false,
-  });
+  const option = useMemo(() => {
+    const root = { refetchOnWindowFocus: false };
+
+    if (initialData) return { ...root, initialData: JSON.parse(initialData) };
+    return root;
+  }, [initialData]);
+
+  const { data: eventData } = useQuery(
+    "fetchEventData",
+    () => getEventData(),
+    option
+  );
 
   return eventData;
 }
