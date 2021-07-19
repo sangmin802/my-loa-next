f14fac06e1b04a4e705f0b873208ecd47f87ef2e
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-19 18:04:16 +0900

    styled-components 초기 흰 화면 문제 해결

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,31 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="stylesheet" href="/fonts/fonts.css" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            <link rel="stylesheet" href="/fonts/fonts.css" />
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
-
-export default MyDocument;
